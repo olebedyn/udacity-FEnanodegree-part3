288162aee137a753331f744ec1df5ca6c7ef20d5
Oleg Lebedyn <olebedyn@cogniance.com>
2018-10-07 17:58:41 +0300

    finished up the game flows

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+const WIN_GAME_CARDS_NUMBER = 16;
+const FIVE_STAR_MOVES_NUMBER = 12;
+const RATING_CHANGE_STEP = 3;
+const ZERO_STAR_MOVES_NUMBER = 12 + 5 * RATING_CHANGE_STEP;
+
 let cards = ['fa-android', 'fa-android', 'fa-angular', 'fa-angular', 'fa-apple', 'fa-apple', 'fa-aws', 'fa-aws', 'fa-chrome', 'fa-chrome', 'fa-docker', 'fa-docker', 'fa-linux', 'fa-linux', 'fa-windows', 'fa-windows']
 let clickedCards = [];
+let gameStarted = false;
+let gameTimeInterval = 0; // remember interval id to be able to clear it when game ends
+let gameStartTimestamp = 0;
+let movesNumber = 0;
 
 // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
 function shuffleCards(a) {
@@ -11,51 +20,61 @@ function shuffleCards(a) {
 }
 
 function showCard() {
+  // start timer when first card in clicked and game starts
+  if (!gameStarted) {
+    gameStarted = true;
+    gameStartTimestamp = Math.floor(Date.now() / 1000);
+    gameTimeInterval = setInterval( () => {
+      let elapsedTime = Math.floor(Date.now() / 1000) - gameStartTimestamp;
+      document.getElementById("timer").innerHTML = "Time: " + Math.floor(elapsedTime / 60) + 'm '+ (elapsedTime - Math.floor(elapsedTime / 60) * 60) + "s";
+    },
+      1000
+    );
+  }
 
   // prevent from 3rd cell being opened
   if (clickedCards.length === 2) {
     return;
   }
+  else {
+    // show card and temorarily remove onclick event to card can not be clicked when opened
+    this.firstChild.classList.add('is-flipped')
+    this.removeEventListener('click', showCard);
+    clickedCards.push(this);
+    // if 2nd card is opened - check whether cards martch
+    if (clickedCards.length === 2) {
+      movesNumber++;
+      updateRating();
+      let match = cardsMatch(clickedCards[0], clickedCards[1]);
+      //leave cards opened if matched
+      if (match){
+        for (card of clickedCards) {
+          setTimeout((card) => {
+            card.getElementsByClassName('cell__face--back')[0].classList.add('cell__face--back--guessed');
+            isGameOver();
+          }, 1000, card);
+        }
+        clickedCards = [];
 
-  // show card and temorarily remove onclick event to card can not be clicked when opened
-  this.firstChild.classList.add('is-flipped')
-  this.removeEventListener('click', showCard);
-  clickedCards.push(this);
-
-
-  // if 2nd card is opened - check whether cards martch
-  if (clickedCards.length === 2) {
-    let match = cardsMatch(clickedCards[0], clickedCards[1]);
-    //leave cards opened if matched
-    if (match){
-      for (card of clickedCards) {
-        setTimeout((card) => {
-          card.getElementsByClassName('cell__face--back')[0].classList.add('cell__face--back--guessed');
-          isGameOver();
-        }, 1000, card);
       }
-      clickedCards = [];
-
-    }
-    // close cards and add on click even back
-    else {
-      for (card of clickedCards) {
-        setTimeout((card) => {
-          card.getElementsByClassName('cell__face--back')[0].classList.add('cell__face--back--wrong');
-        }, 1000, card);
-        setTimeout((card)=> {
-              card.firstChild.classList.remove('is-flipped')
-              card.addEventListener('click', showCard);
-              clickedCards = [];
-              setTimeout((card)=> {
-                card.getElementsByClassName('cell__face--back')[0].classList.remove('cell__face--back--wrong')
-              }, 500, card);
-          }, 2000, card);
+      // close cards and add on click even back
+      else {
+        for (card of clickedCards) {
+          setTimeout((card) => {
+            card.getElementsByClassName('cell__face--back')[0].classList.add('cell__face--back--wrong');
+          }, 1000, card);
+          setTimeout((card)=> {
+                card.firstChild.classList.remove('is-flipped')
+                card.addEventListener('click', showCard);
+                clickedCards = [];
+                setTimeout((card)=> {
+                  card.getElementsByClassName('cell__face--back')[0].classList.remove('cell__face--back--wrong')
+                }, 500, card);
+            }, 2000, card);
+        }
       }
     }
   }
-
-
 }
 
 function cardsMatch(card1, card2) {
@@ -98,12 +117,52 @@ function createBoard(cards) {
 }
 
 function isGameOver() {
-  if (document.getElementsByClassName('cell__face--back--guessed').length === 16) {
-    alert('you won a game');
-    createBoard(shuffleCards(cards));
+  if (document.getElementsByClassName('cell__face--back--guessed').length === WIN_GAME_CARDS_NUMBER) {
+    document.getElementById('popup').classList.add('overlay-visible');
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+function updateRating() {
+  if (movesNumber > FIVE_STAR_MOVES_NUMBER && movesNumber < ZERO_STAR_MOVES_NUMBER ){
+      let numberOfBlankStars = calcNmberOfStars();
+      let stars = document.getElementById('rating').children
+      for (let i=numberOfBlankStars-1; i >= 0; i--){
+        stars[stars.length - 1 - i].classList.replace('fas', 'far');
+      }
+  }
+}
+
+function calcNmberOfStars() {
+  // wil remove 1 stat for each 3 moves above 12
+  return Math.ceil((movesNumber - FIVE_STAR_MOVES_NUMBER) / RATING_CHANGE_STE);
+}
+
+function resetScoreboard(){
+  // Reset timer
+  clearInterval(gameTimeInterval);
+  document.getElementById("timer").innerHTML = "";
+
+  // Rating should be back to 5-star also
+  for (star of document.getElementById("rating").children) {
+    if (star.classList.contains('far')) {
+      star.classList.replace('far', 'fas')
+    }
+  }
+}
+
+function initGame() {
+  if (document.getElementById('popup').classList.contains('overlay-visible'))
+  {
+    document.getElementById('popup').classList.remove('overlay-visible');
+  }
+  clickedCards = [];
+  gameStarted = false;
+  movesNumber = 0;
+  gameStartTimestamp = 0;
+  resetScoreboard();
   createBoard(shuffleCards(cards));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  initGame(cards);
 });
