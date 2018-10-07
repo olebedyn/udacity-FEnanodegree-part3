e2ce6820539eaf29840ecbab03b19d5e5b81769e
Oleg Lebedyn <olebedyn@cogniance.com>
2018-10-07 18:08:52 +0300

    fix few html validator errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    imagemin: {
-      options: {
-        optimizationLevel: 3
-      },
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'img/',
-          src: ['**/*.{png,jpg,gif,jpeg}'],
-          dest: 'img/build'
-        }]
-      }
-    },
-
     validation: {
       options: {
         reset: grunt.option('reset') || true,
@@ -38,12 +24,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-w3c-html-validation');
   grunt.loadNpmTasks('grunt-w3c-css-validation');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['imagemin', 'validation', 'w3c_css_validation']);
+  grunt.registerTask('default', ['validation', 'w3c_css_validation']);
   grunt.registerTask('watch', ['watch']);
 
 };
